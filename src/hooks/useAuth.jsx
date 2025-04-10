8639d6ba3b4962682f06ae057279c5d445de9d34
Manu Chaso <47826459+ManuChaso@users.noreply.github.com>
2025-04-10 12:49:40 +0200

    Release/rr 0003 (#4)
    * feat(RR-0001): Routes && Login component (#1)
    
    * feat(RR-0002): Fetch function && useAuth (#2)
    
    * feat(RR-0003): ProtectedRoute component && not found route (#3)

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import { Api } from "../utils/api";
+
+
+export const useAuth = () => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            setLoading(true)
+            const res = await Api('GET', 'users/profile');
+            //TODO Revisión respuesta
+            setUser(res.result);
+            setLoading(false)
+        }
+
+        fetchUser();
+    }, []);
+
+    return [user, loading];
+}
