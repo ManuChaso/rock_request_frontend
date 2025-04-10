08001b7f287058c62c5fee7316bae199841a74df
Manu Chaso <47826459+ManuChaso@users.noreply.github.com>
2025-04-10 12:18:03 +0200

    feat(RR-0002): Fetch function && useAuth (#2)

---
+++ src/utils/api.js
@@ -0,0 +1,30 @@
+
+
+/**
+ * 
+ * @param {string} method - HTTP METHOD
+ * @param {string} endpoint - API ENDPOINT
+ * @param {object} data - DATA TO FETCH
+ * @returns 
+ */
+export const Api = async (method, endpoint, data) => {
+    try {
+        const config = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                credentials: true
+            }
+        }
+
+        data && (config.body = JSON.stringify(data));
+
+        const response = await fetch(`http://localhost:3000/${endpoint}`, config);
+        
+        const res = await response.json();
+
+        return res
+    } catch (error) {
+        console.error("Fetch error:", error);
+    }
+}
