c2f5f49bbff7dce74701ff7f2e0846e7634fa375
Manu Chaso <47826459+ManuChaso@users.noreply.github.com>
2025-04-10 12:36:09 +0200

    feat(RR-0003): ProtectedRoute component && not found route (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Login } from './components/Login/Login'
+import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute'
 
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path='/' element={<h>Home</h>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/prueba' element={<ProtectedRoute><p>Protected</p></ProtectedRoute>}/>
+        {/*TODO Not found component */}
+        <Route path='/*' element={<p>Not found</p>}/>
       </Routes>
     </>
   )
